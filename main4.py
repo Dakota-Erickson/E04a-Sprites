a7a5568365d0cf6276964ddc6c50e8ad0a20aee7
Erickson <erickdak@iu.edu>
2019-09-18 17:10:52 -0400

    added assets and completed excercise

--- main4.py
+++ main4.py
@@ -15,15 +15,65 @@ class MyGame(arcade.Window):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
-        arcade.set_background_color(open_color.white)
+        arcade.set_background_color(open_color.blue_5)
+        self.animal_list = arcade.SpriteList()
 
 
     def setup(self):
-        pass        
+        self.animal_sprite = arcade.Sprite("assets/ship (1).png", 0.5)
+        self.animal_sprite.center_x = 500
+        self.animal_sprite.center_y = 50
+        self.animal_list.append(self.animal_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/ship (2).png", 0.5)
+        self.animal_sprite.center_x = 500
+        self.animal_sprite.center_y = 150
+        self.animal_list.append(self.animal_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/ship (3).png", 0.5)
+        self.animal_sprite.center_x = 500
+        self.animal_sprite.center_y = 250
+        self.animal_list.append(self.animal_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/ship (4).png", 0.5)
+        self.animal_sprite.center_x = 500
+        self.animal_sprite.center_y = 350
+        self.animal_list.append(self.animal_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/ship (5).png", 0.5)
+        self.animal_sprite.center_x = 500
+        self.animal_sprite.center_y = 450
+        self.animal_list.append(self.animal_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/ship (6).png", 0.5)
+        self.animal_sprite.center_x = 200
+        self.animal_sprite.center_y = 50
+        self.animal_list.append(self.animal_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/ship (7).png", 0.5)
+        self.animal_sprite.center_x = 200
+        self.animal_sprite.center_y = 150
+        self.animal_list.append(self.animal_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/ship (8).png", 0.5)
+        self.animal_sprite.center_x = 200
+        self.animal_sprite.center_y = 250
+        self.animal_list.append(self.animal_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/ship (9).png", 0.5)
+        self.animal_sprite.center_x = 200
+        self.animal_sprite.center_y = 350
+        self.animal_list.append(self.animal_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/ship (10).png", 0.5)
+        self.animal_sprite.center_x = 200
+        self.animal_sprite.center_y = 450
+        self.animal_list.append(self.animal_sprite)
 
     def on_draw(self):
         arcade.start_render()
-        pass
+        self.animal_list.draw()
+
 
 
     def update(self, delta_time):
